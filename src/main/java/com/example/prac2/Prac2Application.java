7d0e860f2b5bccd52f3e56dab87bfd5c22db7af0
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-07-06 11:29:42 +0900

    17

--- src/main/java/com/example/prac2/Prac2Application.java
+++ src/main/java/com/example/prac2/Prac2Application.java
@@ -13,7 +13,7 @@ public class Prac2Application {
         System.out.println("hihi");
         System.out.println("hihi1");
         System.out.println("bye222");
-        System.out.println("hi122345678");
+        System.out.println("hi1223456782");
     }
 
 }
